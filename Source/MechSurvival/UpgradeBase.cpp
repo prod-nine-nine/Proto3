5ac143f0a8b94bd5d2e951f8b28f1bd8215f27ad
rhynz <rhynz@hotmail.co.nz>
2020-03-18 14:09:22 +1300

    mech now needs each part enabled

---
+++ Source/MechSurvival/UpgradeBase.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UpgradeBase.h"
+
+// Sets default values
+AUpgradeBase::AUpgradeBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	//PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AUpgradeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AUpgradeBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
